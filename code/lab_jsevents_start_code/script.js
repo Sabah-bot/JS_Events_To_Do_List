93b63907dd494681617f82ebe635670963a023e9
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-06-24 14:56:31 +0100

    added enter and made list work

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,19 +1,12 @@
 const textInput = document.querySelector("#todo-form");
 const button = document.querySelector("#enter");
+const list = document.querySelector("#list");
 
 
-button.addEventListener("click", () => {
-    console.log("button clicked");
-});
-
-// textInput.addEventListener("input", (event) => {
-//     textOutput.innerText = event.target.value; 
-//  });
-
-
-button.addEventListener("click", (event)=> {
+textInput.addEventListener("submit", (event)=> {
+console.log(event);
 event.preventDefault();
-const newListItem = document.createElement("li");
-newListItem.innerText = event.target["new-todo"].value;           
-toDolist.appendChild(newListItem);  
+const newListItem = document.createElement('li');
+newListItem.innerText = event.target['new-todo'].value;           
+list.appendChild(newListItem);  
 })
