310c479bab211c6dce453397b1cda8b47e355d4d
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-06-24 15:21:54 +0100

    delete button updates

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -9,12 +9,15 @@ textInput.addEventListener("submit", (event)=> {
 console.log(event);
 event.preventDefault();
 const newListItem = document.createElement('li');
-newListItem.innerText = event.target['new-todo'].value;           
+newListItem.innerText = event.target['new-todo'].value;      
+const deletebutton = document.createElement("button");
+const buttonText = document.createTextNode("DELETE");
 list.appendChild(newListItem);  
+list.appendChild(deletebutton);
 })
 
 deletebutton.addEventListener("click",(event)=> {
     // newListItem = event.target['new-todo'].value;           
-    list.removeChild(lastChild);  
+    list.removeChild(list.lastChild);  
   
 })
