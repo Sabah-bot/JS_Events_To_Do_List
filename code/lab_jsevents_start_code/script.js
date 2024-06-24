6282b864b714e3c109b9d32f4e0cba7e933cc4f2
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-06-24 15:14:04 +0100

    delete button mid

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,6 +1,8 @@
 const textInput = document.querySelector("#todo-form");
 const button = document.querySelector("#enter");
 const list = document.querySelector("#list");
+const deletebutton = document.querySelector("#delete");
+
 
 
 textInput.addEventListener("submit", (event)=> {
@@ -9,4 +11,10 @@ event.preventDefault();
 const newListItem = document.createElement('li');
 newListItem.innerText = event.target['new-todo'].value;           
 list.appendChild(newListItem);  
+})
+
+deletebutton.addEventListener("click",(event)=> {
+    // newListItem = event.target['new-todo'].value;           
+    list.removeChild(lastChild);  
+  
 })
