68d5f2e28ae16d7e31db2e6cf410e069488a93db
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-06-24 15:37:32 +0100

    added some organisation bits

--- code/lab_jsevents_start_code/script.js
+++ code/lab_jsevents_start_code/script.js
@@ -1,23 +1,34 @@
 const textInput = document.querySelector("#todo-form");
+const userInput = document.querySelector("#new-todo");
 const button = document.querySelector("#enter");
 const list = document.querySelector("#list");
 const deletebutton = document.querySelector("#delete");
+const dateButton = document.querySelector("#show-date")
 
 
-
+// `Enter` button +`delete` button + allows a user to delete an item from the list 
 textInput.addEventListener("submit", (event)=> {
 console.log(event);
 event.preventDefault();
 const newListItem = document.createElement('li');
-newListItem.innerText = event.target['new-todo'].value;      
 const deletebutton = document.createElement("button");
-const buttonText = document.createTextNode("DELETE");
+const buttonText = document.createElement("DELETE");
+newListItem.innerText = event.target['new-todo'].value;      
+deletebutton.appendChild(buttonText);
+
 list.appendChild(newListItem);  
 list.appendChild(deletebutton);
-})
 
 deletebutton.addEventListener("click",(event)=> {
-    // newListItem = event.target['new-todo'].value;           
-    list.removeChild(list.lastChild);  
-  
-})
+    list.removeChild(list.lastChild); 
+});
+
+});
+
+
+// Create a `Show Date` button +added directly to `index.html`. (hint: `Date()` returns today's date and time) 
+
+// dateButton.addEventListener("click", () => {
+
+// })
+
